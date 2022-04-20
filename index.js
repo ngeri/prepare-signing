0e444b96f5eb04e388cbbd9e7d36b5d8b89dbace
Sam Lievesley <74614825+SamLievesley-TomTom@users.noreply.github.com>
2022-04-20 19:49:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,8 @@ function setupKeychain(keychainName, keychainPassword, base64P12File, p12Passwor
 
 async function run() {
   try {
-
+    console.log('Hello, world!');
+    
     const appStoreConnectPrivateKey = core.getInput(`appStoreConnectPrivateKey`);
     const keyID = core.getInput(`keyID`);  
     const issuerID = core.getInput(`issuerID`);
