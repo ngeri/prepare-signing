0796e43a06635489cdb4be048020956a27eee65a
Sam Lievesley <74614825+SamLievesley-TomTom@users.noreply.github.com>
2022-04-20 20:49:02 +0200

    Remove log line

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ async function run() {
     const token = getToken(issuerID, 2, Buffer.from(appStoreConnectPrivateKey, "utf8"), keyID);
     const bundleIdResponse = await get("https://api.appstoreconnect.apple.com/v1/bundleIds", { "filter[identifier]": bundleIdentifier }, token); // BundleIdsResponse Type
     const bundleId = bundleIdResponse.data.find(element => element.attributes.identifier == bundleIdentifier);
-    console.log(bundleId);
     if (bundleId) {
       const profileIds = await get(`https://api.appstoreconnect.apple.com/v1/bundleIds/${bundleId.id}/relationships/profiles`, { }, token);  
       const rawProfileIds = profileIds.data.map(profile => profile.id);
